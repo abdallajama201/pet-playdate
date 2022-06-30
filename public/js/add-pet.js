6309cb291c611412cc54b60d08ee7bf4314b11ce
DLittlefield81 <dennis.ah.littlefield@gmail.com>
2022-06-30 14:35:50 -0400

    add pet func works, work on remove next

---
+++ public/js/add-pet.js
@@ -0,0 +1,28 @@
+const addPet = async (event) => {
+    event.preventDefault();
+
+    const pet_name = document.querySelector('#PetName').value.trim();
+    const breed = document.querySelector('#PetBreed').value.trim();
+    const age = document.querySelector('#PetAge').value.trim();
+
+    if (pet_name && breed && age) {
+        const response = await fetch(`/api/pets`, {
+            method: 'POST',
+            body: JSON.stringify({ pet_name, breed, age }),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+
+        if (response.ok) {
+            document.location.replace('/profile');
+        } else {
+            alert('Failed to create project');
+        }
+    }
+};
+
+
+document
+    .querySelector('#add-pet')
+    .addEventListener('click', addPet);

93d6e2b66d3872625652c57b0df617543051ae4e
Abdalla Jama <abdallajama201@gmail.com>
2022-07-04 17:27:59 -0400

    added seeding functions

---
+++ seeds/seed.js
@@ -0,0 +1,53 @@
+const { faker } = require('@faker-js/faker');
+const { Model } = require('sequelize');
+const { User, PlayDate, Pet } = require("../models");
+const { sequelize } = require('../models/User');
+
+function createUser() {
+    return {
+        user_name: faker.name.firstName(),
+        email: faker.internet.email(),
+        password: "password"
+    };
+};
+
+function createEvent(id) {
+    return {
+        date: faker.date.future(),
+        time: "00:00:00",
+        location: faker.address.city(),
+        user_id: id
+    }
+}
+
+function createDog(id) {
+    return {
+        pet_name: faker.name.firstName(),
+        breed: faker.animal.dog(),
+        age: Math.floor(Math.random()*15)+1,
+        user_id: id
+    }
+}
+
+sequelize.sync({ force: false }).then(() => {
+    for(let i = 0; i < 10; i++) {
+        async function createData() {
+            let user = await User.create(createUser());
+            let dog1 = await Pet.create(createDog(user.dataValues.id))
+            let dog2 = await Pet.create(createDog(user.dataValues.id))
+            let event1 = await PlayDate.create(createEvent(user.dataValues.id))
+            let event2 = await PlayDate.create(createEvent(user.dataValues.id))
+            
+            const user1 = await User.findByPk(user.dataValues.id, {
+            include:[{model: Pet}, {model: PlayDate}]
+            });
+            const playDate1 = user1.playdates[user1.playdates.length - 1]
+            const playDate2 = user1.playdates[user1.playdates.length - 2]
+            for(let i = 0;i < user1.pets.length; i++) {
+                await playDate1.addPet(user1.pets[i]);
+                await playDate2.addPet(user1.pets[i]);
+            }
+    }
+    createData();
+    }
+});
